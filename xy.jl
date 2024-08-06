fa8f9c8abdb9c7ee3808b8dd344c9f380a661d4a
Kiize <paolozzobibi@gmail.com>
2024-08-06 14:32:40 +0200

    Init repo

---
+++ xy.jl
@@ -0,0 +1,72 @@
+using ITensors
+using Statistics
+using Plots
+
+# Inizializzazione.
+
+N = 100 # Numero siti.
+sites = siteinds("Fermion", N) # N Fermions.
+
+####################################### Costruiamo l'hamiltoniana XY.
+"""
+function H_xy(N)
+    os = OpSum()
+    for i in 1:N-1
+        os += "Cdag", i, "C", i+1
+        os += "C", i, "Cdag", i+1
+    end 
+
+    return os
+end
+
+Hxy = H_xy(N)
+"""
+####################################### Costruiamo l'hamiltoniana XX (vedi Sachdev 16.1).
+function H_xx(N, h)
+    os = OpSum()
+    for i in 1:N-1
+        os -= "Cdag", i, "C", i+1
+        os -= "C", i, "Cdag", i+1
+        os -= h,"Cdag",i,"C",i  # h = μ/w
+    end 
+
+    os -= h,"Cdag",N,"C",N
+
+    return os
+end
+
+len = 100
+occupation_numbers = zeros(len, N)
+x = range(start = -3., stop = 3., length = len)
+
+for (ind, h) in enumerate(x)
+    Hxx = H_xx(N, h)
+    H = MPO(Hxx,sites)  # Convertiamo H in un Matrix Product Operator.
+    
+    # DMRG. 
+    
+    psi0 = random_mps(sites;linkdims=10)
+    
+    nsweeps = 7
+    maxdim = [10,20,100,100,200,400]
+    cutoff = [1E-10]
+    
+    energy,psi = dmrg(H,psi0;nsweeps,maxdim,cutoff)
+    
+    occupation_numbers[ind, :] = expect(psi, "N") # Calcoliamo <N> dove N è l'operatore densità.
+end
+
+y = mean(occupation_numbers, dims = 2)
+plot(x, y)
+
+function theoretic_value(h)
+    if h <= -2
+        return 0
+    elseif h>= 2
+        return 1
+    else
+        return 1 - 1 / pi * acos(h/2)
+    end
+end
+
+plot!(x, theoretic_value.(x))
